512c77eddbd179423bafd9cfb6a100dcb3b7b616
Jiwon Choi <jjiwoning12@gmail.com>
2023-05-03 16:26:37 +0900

    feat: TripTeam OneToMany Mapping

--- enjoytrip/src/main/java/com/ssafy/enjoytrip/domain/TripTeam.java
+++ enjoytrip/src/main/java/com/ssafy/enjoytrip/domain/TripTeam.java
@@ -1,21 +1,42 @@
 package com.ssafy.enjoytrip.domain;
 
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
-@Builder
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class TripTeam extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long tripTeamId;
+
     private String teamName;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User master;
+
+    @OneToMany(mappedBy = "tripTeam", cascade = CascadeType.ALL)
+    private List<UserTripTeam> userTripTeams = new ArrayList<>();
+
+    @Builder
+    public TripTeam(Long tripTeamId, String teamName, User master) {
+        this.tripTeamId = tripTeamId;
+        this.teamName = teamName;
+        this.master = master;
+    }
+
+    // 연관 관계 편의 메서드
+    public void addUserTripTeam(UserTripTeam userTripTeam) {
+        userTripTeams.add(userTripTeam);
+        userTripTeam.addTripTeam(this);
+    }
 }
