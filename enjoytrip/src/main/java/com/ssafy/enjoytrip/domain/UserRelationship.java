2b80d50d482e92a29091301ddb722f4dad543638
Jiwon Choi <jjiwoning12@gmail.com>
2023-05-03 17:02:49 +0900

    fix: UserRelationship add NoArgsConstructor

--- enjoytrip/src/main/java/com/ssafy/enjoytrip/domain/UserRelationship.java
+++ enjoytrip/src/main/java/com/ssafy/enjoytrip/domain/UserRelationship.java
@@ -1,14 +1,16 @@
 package com.ssafy.enjoytrip.domain;
 
 import com.ssafy.enjoytrip.domain.user_relation.Relation;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Getter
-@Builder
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserRelationship extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,4 +26,12 @@ public class UserRelationship extends BaseTimeEntity {
 
     @Enumerated(EnumType.STRING)
     private Relation relation; // FOLLOW, BLOCK
+
+    @Builder
+    public UserRelationship(Long userRelationshipId, User user, User targetUser, Relation relation) {
+        this.userRelationshipId = userRelationshipId;
+        this.user = user;
+        this.targetUser = targetUser;
+        this.relation = relation;
+    }
 }
