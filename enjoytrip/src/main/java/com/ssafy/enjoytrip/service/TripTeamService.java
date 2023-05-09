cc596f96fec83f8a37a7c387e442baee9d11ebad
Jiwon Choi <jjiwoning12@gmail.com>
2023-05-09 16:59:43 +0900

    feat: makeTeam and invite User

--- enjoytrip/src/main/java/com/ssafy/enjoytrip/service/TripTeamService.java
+++ enjoytrip/src/main/java/com/ssafy/enjoytrip/service/TripTeamService.java
@@ -1,4 +1,11 @@
 package com.ssafy.enjoytrip.service;
 
+import com.ssafy.enjoytrip.domain.TripTeam;
+
 public interface TripTeamService {
+    void makeTripTeam(Long userId, String teamName);
+
+    TripTeam findTripTeam(Long tripTeamId);
+
+    void addTeamMember(Long userId, Long teamId);
 }
--- enjoytrip/src/main/java/com/ssafy/enjoytrip/service/TripTeamServiceImpl.java
+++ enjoytrip/src/main/java/com/ssafy/enjoytrip/service/TripTeamServiceImpl.java
@@ -1,6 +1,10 @@
 package com.ssafy.enjoytrip.service;
 
+import com.ssafy.enjoytrip.domain.TripTeam;
+import com.ssafy.enjoytrip.domain.User;
+import com.ssafy.enjoytrip.domain.UserTripTeam;
 import com.ssafy.enjoytrip.repository.TripTeamRepository;
+import com.ssafy.enjoytrip.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -10,8 +14,36 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class TripTeamServiceImpl {
+public class TripTeamServiceImpl implements TripTeamService {
 
     private final TripTeamRepository tripTeamRepository;
 
+    private final UserRepository userRepository;
+
+    @Override
+    public void makeTripTeam(Long userId, String teamName) {
+        User findUser = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 사용자"));
+        TripTeam tripTeam = TripTeam.builder().teamName(teamName).master(findUser).build();
+        tripTeamRepository.save(tripTeam);
+    }
+
+    @Override
+    public TripTeam findTripTeam(Long tripTeamId) {
+        return tripTeamRepository.findById(tripTeamId)
+                .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 팀"));
+    }
+
+    @Override
+    public void addTeamMember(Long userId, Long teamId) {
+        TripTeam tripTeam = tripTeamRepository.findById(teamId)
+                .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 팀"));
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 사용자"));
+
+        UserTripTeam userTripTeam = UserTripTeam.builder().tripTeam(tripTeam).user(user).build();
+
+        tripTeam.addUserTripTeam(userTripTeam);
+    }
 }
