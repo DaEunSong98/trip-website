da81538f1f51475110e32b323915332217c4566d
Jiwon Choi <jjiwoning12@gmail.com>
2023-05-16 17:25:48 +0900

    fix: cors 이슈 해결

--- enjoytrip/src/main/java/com/ssafy/enjoytrip/EnjoytripApplication.java
+++ enjoytrip/src/main/java/com/ssafy/enjoytrip/EnjoytripApplication.java
@@ -2,7 +2,10 @@ package com.ssafy.enjoytrip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -12,4 +15,13 @@ public class EnjoytripApplication {
 		SpringApplication.run(EnjoytripApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigure() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://127.0.0.1:5500", "http://127.0.0.1:9001", "http://localhost:9001");
+			}
+		};
+	}
 }
