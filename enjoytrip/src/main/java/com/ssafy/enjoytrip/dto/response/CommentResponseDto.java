2de269baeccfb5409e85b31ddc39fcc8c1f48ea4
Jiwon Choi <jjiwoning12@gmail.com>
2023-05-17 17:14:49 +0900

    fix: CommentResponseDto에 작성일 추가

--- enjoytrip/src/main/java/com/ssafy/enjoytrip/dto/response/CommentResponseDto.java
+++ enjoytrip/src/main/java/com/ssafy/enjoytrip/dto/response/CommentResponseDto.java
@@ -3,6 +3,8 @@ package com.ssafy.enjoytrip.dto.response;
 import com.ssafy.enjoytrip.domain.Comment;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class CommentResponseDto {
 
@@ -10,8 +12,11 @@ public class CommentResponseDto {
 
     private String nickname;
 
+    private LocalDateTime createdDate;
+
     public CommentResponseDto(Comment comment) {
         this.content = comment.getContent();
         this.nickname = comment.getUser().getNickname();
+        this.createdDate = comment.getCreatedDate();
     }
 }
