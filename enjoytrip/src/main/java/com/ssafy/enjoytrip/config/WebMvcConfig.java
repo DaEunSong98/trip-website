f2b316404ccf87605b997413a5e876840274499d
Jiwon Choi <jjiwoning12@gmail.com>
2023-05-22 13:15:45 +0900

    fix: cors 메서드(PATCH) 적용 안되는 이슈 해결

--- enjoytrip/src/main/java/com/ssafy/enjoytrip/config/WebMvcConfig.java
+++ enjoytrip/src/main/java/com/ssafy/enjoytrip/config/WebMvcConfig.java
@@ -3,6 +3,7 @@ package com.ssafy.enjoytrip.config;
 import com.ssafy.enjoytrip.token.interceptor.TokenInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -25,7 +26,15 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOriginPatterns("*")
-                .allowedMethods("*")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name(),
+                        HttpMethod.PATCH.name(),
+                        HttpMethod.HEAD.name(),
+                        HttpMethod.OPTIONS.name()
+                )
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
