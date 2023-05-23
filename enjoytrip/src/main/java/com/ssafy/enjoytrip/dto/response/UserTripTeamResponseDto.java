156c68389c69549c721995cd6f6ef61405219359
Jiwon Choi <jjiwoning12@gmail.com>
2023-05-23 11:43:39 +0900

    fix: TripPlan에 Default 생성자 추가

--- enjoytrip/src/main/java/com/ssafy/enjoytrip/dto/response/UserTripTeamResponseDto.java
+++ enjoytrip/src/main/java/com/ssafy/enjoytrip/dto/response/UserTripTeamResponseDto.java
@@ -7,11 +7,14 @@ import lombok.Data;
 @Data
 public class UserTripTeamResponseDto {
 
-    String nickname;
+    private Long id;
+
+    private String nickname;
 
     private TeamRole teamRole;
 
     public UserTripTeamResponseDto(UserTripTeam userTripTeam) {
+        this.id = userTripTeam.getUserTripTeamId();
         this.nickname = userTripTeam.getUser().getNickname();
         this.teamRole = userTripTeam.getTeamRole();
     }
