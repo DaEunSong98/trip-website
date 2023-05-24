b031a543fdbf824a9f5c2adfbe298a81daacd185
Jiwon Choi <jjiwoning12@gmail.com>
2023-05-24 17:34:23 +0900

    refactor: TripTeamListResponse 응답 필드 추가

--- enjoytrip/src/main/java/com/ssafy/enjoytrip/dto/response/TripTeamListResponse.java
+++ enjoytrip/src/main/java/com/ssafy/enjoytrip/dto/response/TripTeamListResponse.java
@@ -1,6 +1,7 @@
 package com.ssafy.enjoytrip.dto.response;
 
 import com.ssafy.enjoytrip.domain.TripTeam;
+import com.ssafy.enjoytrip.domain.team_relation.TeamRole;
 import lombok.Data;
 
 @Data
@@ -10,8 +11,11 @@ public class TripTeamListResponse {
 
     private String teamName;
 
+    private TeamRole teamRole;
+
     public TripTeamListResponse(TripTeam tripTeam) {
         this.tripTeamId = tripTeam.getTripTeamId();
         this.teamName = tripTeam.getTeamName();
+        this.teamRole = tripTeam.getUserTripTeams().get(0).getTeamRole();
     }
 }
